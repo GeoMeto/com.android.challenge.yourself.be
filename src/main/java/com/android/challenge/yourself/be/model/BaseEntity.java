de9cca37f474d5c9b389d93e066c3e50fa2f6678
Georgi Metodiev <georgi.metodiev@effortel.com>
2022-06-15 23:30:14 +0300

    Adds jsonignore

--- src/main/java/com/android/challenge/yourself/be/model/BaseEntity.java
+++ src/main/java/com/android/challenge/yourself/be/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.android.challenge.yourself.be.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
@@ -16,9 +17,12 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+    @JsonIgnore
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @JsonIgnore
     @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
